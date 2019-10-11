c7c4cada3dde71450b531ae954ebc001b8f0e414
IgnacyPawlak <ignacy.pawlak@gmail.com>
2019-10-11 21:06:44 +0200

    switchcase

--- Podstawy_Programowania/2_zajecia/SwitchCasePetlaFor/ConsoleApp1/Program.cs
+++ Podstawy_Programowania/2_zajecia/SwitchCasePetlaFor/ConsoleApp1/Program.cs
@@ -10,7 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello World!");
+            Console.WriteLine("1 - program do obliczania pola prostokąta\n2 - program do obliczania pola koła\nWybierz który program chcesz użyć. Wprowadź liczbę");
+            string choice = Console.ReadLine();
+            double choice1;
+            switch (choice)
+            {
+                case "1":
+                    {
+                        if (double.TryParse(choice, out choice1))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Wybrałeś program do obliczania pola prostokąta.\nPodaj długości boków");
+                            double a = double.Parse(Console.ReadLine());
+                            double b = double.Parse(Console.ReadLine());
+                            double wynik = a * b;
+                            Console.WriteLine("Pole prostokąta wynosi: " + wynik);
+
+
+                        }
+                        break;
+                    }
+                case "2":
+                    {
+                        if (double.TryParse(choice, out choice1))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Wybrałeś program do obliczenia pola koła.\nPodaj długość promienia: ");
+                            double a = double.Parse(Console.ReadLine());
+                            double wynik = a * a * Math.PI;
+                            Console.WriteLine("Pole tego koła wyni: {0}", wynik);
+
+                        }
+                        break;
+                    }
+                default:
+                    Console.WriteLine("zły format liczby");
+                    break;
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
