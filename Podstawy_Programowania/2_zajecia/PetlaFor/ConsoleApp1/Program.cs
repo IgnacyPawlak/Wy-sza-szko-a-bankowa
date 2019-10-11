5768217b9f6182eeaa7bab633db5cf886e000e86
IgnacyPawlak <ignacy.pawlak@gmail.com>
2019-10-11 21:49:42 +0200

    musze poprawić odliczanie od 20 do zera parzyscie

---
+++ Podstawy_Programowania/2_zajecia/PetlaFor/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program pokazuje liczby parzyste z przedziału od 20 do 0");
+            int x = 1;
+            for (int i = 20;  i % 2 == 0 ; x++)
+            Console.Write(i + " ");
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
