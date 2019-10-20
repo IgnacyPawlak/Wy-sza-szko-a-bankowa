8f930fd78b7e12c5af4e5b70d2ae4696437320a5
IgnacyPawlak <ignacy.pawlak@gmail.com>
2019-10-20 15:00:42 +0200

    cslabs

---
+++ zadania CSLabs/zadanie 1/zadanie 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 1.\nProgram oblicza wartość wyrażenia (a * a + b)/((a + b) * (a + b)).\nPodaj zmienną a, wciśnij enter, a następnie podaj zmienną b i wciśnij enter.");
+            string inputa = Console.ReadLine();
+            string inputb = Console.ReadLine();            
+            float a = float.Parse(inputa);
+            float b = float.Parse(inputb);
+            float wynik;
+            wynik = (a * a + b) / ((a + b) * (a + b));
+            Console.WriteLine("Twój wynik to {0}", wynik);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadania CSLabs/zadanie2/zadanie2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 2.\nProgram obliczający wartości wyrażeń.");
+            Console.Write("Podaj a: ");
+            string inputa = Console.ReadLine();
+            Console.Write("Podaj b: ");
+            string inputb = Console.ReadLine();
+            Console.Write("Podaj c:: ");
+            string inputc = Console.ReadLine();
+            double a = double.Parse(inputa);
+            double b = double.Parse(inputb);
+            double c = double.Parse(inputc);
+            double wynik1, wynik2, wynik3;
+            wynik1 = a * a + b;
+
+        }
+    }
+}
